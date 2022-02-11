c94c9894e6c7d0a5e4c4a6154d59908536a13c65
Kenny Rogers <kenneth.rogers@fda.hhs.gov>
2022-02-11 02:39:55 +0000

    refactor: static server directories

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(port, () => {
