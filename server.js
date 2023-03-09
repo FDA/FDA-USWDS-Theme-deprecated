2e951079b3f8d6da8550ed32e80f66a5501da440
Kenny Rogers <kenneth.rogers@fda.hhs.gov>
2023-03-09 15:49:42 -0500

    fix: livereload connect middleware

--- server.js
+++ server.js
@@ -7,19 +7,15 @@ const port = 3000
 const liveReloadPort = 35729
 
 // load static content first
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'dist')))
 
-// load liveReload script only in development mode
-app.configure('development', function() {
-  // live reload script
-  let excludeList = ['.woff', '.flv'];
-  
-  app.use(require('connect-livereload')({
-    port: liveReloadPort,
-    excludeList: excludeList
-  }));
-});
+// live reload script
+app.use(require('connect-livereload')({
+  port: liveReloadPort
+}));
+
+// static html files
+app.use(express.static(path.join(__dirname, 'public')))
 
 // start the server
 const server = app.listen(port, () => {
