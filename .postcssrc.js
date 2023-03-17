ee1dedb62db124d18adefcf4013d01e80a0076d7
Kenny Rogers <kenneth.rogers@fda.hhs.gov>
2023-03-17 13:24:48 -0400

    build: configure storybook, postcsso

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,9 +1,9 @@
 module.exports = (ctx) => {
-    const options = ctx.options;
+    const options = ctx.options ?? {}
     return {
         plugins: {
             'autoprefixer': { cascade: false },
-            'postcss-csso': options.run.css.minify ? { forceMediaMerge: false } : false,
+            'postcss-csso': options.run?.css.minify ? { forceMediaMerge: false } : false,
         }
     }
 }
